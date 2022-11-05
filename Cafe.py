6eef9a3b42fc1e9b98f20960778ec84c47e632e8
Королев Артем <1371qetuop@gmail.com>
2022-11-05 11:28:13 -0400

    Cafe.max_happiness_of() and Cafe.total_hap_of() added; eGreedy() done

--- Cafe.py
+++ Cafe.py
@@ -7,7 +7,33 @@ class Cafe():
         self.std_dev = standard_deviation
         self.visit_history = []
 
+
     def visit(self) -> float:
         happiness = random.normalvariate(self.avg_hpp, self.std_dev)
         self.visit_history.append(happiness)
         return happiness
+
+    @classmethod
+    def max_happiness_of(cls, cafes:list):
+        """
+        :param cafes: a list of cafes (class Cafe)
+        :return: cafe with the most total happiness
+        """
+        best = cafes[0]
+        for cafe in cafes:
+            if sum(cafe.visit_history) > sum(best.visit_history):
+                best = cafe
+
+        return cafe
+
+    @classmethod
+    def total_hap_of(cls, cafes:list):
+        """
+        :param cafes: a list of cafes (class Cafe)
+        :return: total happiness of all cafes
+        """
+        s = 0.0
+        for cafe in cafes:
+            s += sum(cafe.visit_history)
+
+        return s
---
+++ Egreedy.py
@@ -0,0 +1,23 @@
+from Cafe import Cafe
+from random import randint
+
+
+def eGreedy(e=10):
+    cafe1, cafe2, cafe3 = Cafe(10, 8), Cafe(15, 6), Cafe(12, 5)
+
+    cafe1.visit()
+    cafe2.visit()
+    cafe3.visit()
+
+    for _ in range(297):
+        most_hap = Cafe.max_happiness_of([cafe1, cafe2, cafe3])
+        goto = randint(0, 101)
+        if goto <= e:
+            gt = randint(0, 2)
+            while [cafe1, cafe2, cafe3][gt] is most_hap:
+                gt = randint(0, 2)
+            [cafe1, cafe2, cafe3][gt].visit()
+        elif goto > e:
+            most_hap.visit()
+
+    return Cafe.total_hap_of([cafe1, cafe2, cafe3])
